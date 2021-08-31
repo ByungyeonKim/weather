595f01452be83df4cd65046bda8f3d4c33bf5005
ByungyeonKim <viva_kby@naver.com>
2021-08-31 16:02:21 +0900

    Fix: clothes.js

--- src/clothes.js
+++ src/clothes.js
@@ -1,10 +1,9 @@
 export default function clothes(data) {
-  let clothes = document.querySelector('.today-clothes');
+  let clothes = document.querySelector('.clothes-list');
   let temp = data.main.temp;
 
   if (temp <= 4) {
     return (clothes.innerHTML = `
-        <li><h1>지금 날씨엔..</h1></li>
         <li><h2>패딩</h2></li>
         <li><h2>두꺼운 코트</h2></li>
         <li><h2>목도리</h2></li>
@@ -13,7 +12,6 @@ export default function clothes(data) {
   }
   if (temp >= 5 && temp < 9) {
     return (clothes.innerHTML = `
-        <li><h1>지금 날씨엔..</h1></li>
         <li><h2>코트</h2></li>
         <li><h2>가죽자켓</h2></li>
         <li><h2>발열내의</h2></li>
@@ -23,7 +21,6 @@ export default function clothes(data) {
   }
   if (temp >= 9 && temp < 12) {
     return (clothes.innerHTML = `
-        <li><h1>지금 날씨엔..</h1></li>
         <li><h2>자켓</h2></li>
         <li><h2>트렌치코트</h2></li>
         <li><h2>야상</h2></li>
@@ -34,7 +31,6 @@ export default function clothes(data) {
   }
   if (temp >= 12 && temp < 17) {
     return (clothes.innerHTML = `
-        <li><h1>지금 날씨엔..</h1></li>
         <li><h2>얇은 니트</h2></li>
         <li><h2>맨투맨</h2></li>
         <li><h2>가디건</h2></li>
@@ -43,7 +39,6 @@ export default function clothes(data) {
   }
   if (temp >= 17 && temp < 20) {
     return (clothes.innerHTML = `
-        <li><h1>지금 날씨엔..</h1></li>
         <li><h2>얇은 가디건</h2></li>
         <li><h2>긴팔</h2></li>
         <li><h2>면바지</h2></li>
@@ -52,7 +47,6 @@ export default function clothes(data) {
   }
   if (temp >= 20 && temp < 23) {
     return (clothes.innerHTML = `
-        <li><h1>지금 날씨엔..</h1></li>
         <li><h2>얇은 가디건</h2></li>
         <li><h2>얇은 셔츠</h2></li>
         <li><h2>반바지</h2></li>
@@ -61,7 +55,6 @@ export default function clothes(data) {
   }
   if (temp >= 23 && temp < 28) {
     return (clothes.innerHTML = `
-        <li><h1>지금 날씨엔..</h1></li>
         <li><h2>반팔</h2></li>
         <li><h2>얇은 셔츠</h2></li>
         <li><h2>반바지</h2></li>
@@ -70,7 +63,6 @@ export default function clothes(data) {
   }
   if (temp >= 28) {
     return (clothes.innerHTML = `
-        <li><h1>지금 날씨엔..</h1></li>
         <li><h2>민소매</h2></li>
         <li><h2>반팔</h2></li>
         <li><h2>반바지</h2></li>
